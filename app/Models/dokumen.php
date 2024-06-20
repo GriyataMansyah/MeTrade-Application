3b9a4a2563a18fbe78cd1cfcd4748c0b6ccd0324
Irfan Musthofa <143340795+GriyataMansyah@users.noreply.github.com>
2024-06-20 11:58:20 +0700

    Update dokumen.php

--- app/Models/dokumen.php
+++ app/Models/dokumen.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class dokumen extends Model
+class Dokumen extends Model
 {
     use HasFactory;
 
